c05258bf6f2e13969144851017aa92b1092c749c
Rachel L Wildberger <rachellwildberger@Rachels-MacBook-Pro.local>
2022-12-22 18:38:41 -1000

    display list of recipe ingredients

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,7 +9,7 @@ var liquorType = ["whisky", "vodka", "rum", "tequila", "gin", "brandy"];
 
 var displayCocktailData = function (data) {
 
-    var theCocktailUrl = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?i=';
+    var theCocktailUrl = 'www.thecocktaildb.com/api/json/v1/1/filter.php?i=';
 
     fetch(theCocktailUrl)
         .then(function (response) {
@@ -28,8 +28,8 @@ var displayCocktailData = function (data) {
 
 var getCocktail = function (liquorType) {
 
-    // var liquorType = "rum";
-    var theCocktailUrl = `https://www.thecocktaildb.com/api/json/v1/1/search.php?i=${liquorType}`;
+    var liquorType = "gin";
+    var theCocktailUrl = `www.thecocktaildb.com/api/json/v1/1/filter.php?i=${liquorType}`;
 
     fetch(theCocktailUrl)
         .then(function (response) {
@@ -39,7 +39,6 @@ var getCocktail = function (liquorType) {
             console.log(data);
         });
 };
-;
 
 var getRandom = function (data) {
 
@@ -52,39 +51,43 @@ var getRandom = function (data) {
         .then(function (data) {
             displayRandom(data);
         })
-        .catch(function (error) {
-            alert('Unable to connect to Cocktail API');
-        });
+        // .catch(function (error) {
+        //     alert('Unable to connect to Cocktail API');
+        // });
 };
-;
-
-    var ingredientsList = ["strIngredient1", "strIngredient2", "strIngredient3", "strIngredient4", "strIngredient5", 
-    "strIngredient6", "strIngredient7", "strIngredient8", "strIngredient9", "strIngredient10", "strIngredient11", 
-    "strIngredient12", "strIngredient13", "strIngredient14", "strIngredient15"];
 
 var displayRandom = function (data) {
 
+    var recipe = data.drinks[0];
+
+    var recipeIngredients = Object.entries(recipe);
+    var ingredients = [];
+
+    for (var i = 0; i < recipeIngredients.length; i++) {
+        if (recipeIngredients[i][0].includes("strIngredient") && recipeIngredients[i][1]) {
+            ingredients.push(recipeIngredients[i][1])
+        }
+    }
+    
+    var ingredientsEl = document.getElementById("ingredients");
     var cocktailNameEl = document.getElementById("cocktail-name");
     var instructionsEl = document.getElementById("instructions");
-    var imageEl = document.getElementById("image");
-    var ingredientsEl = document.getElementById("ingredients");
-
-
+    // var imageEl = document.getElementById("image");
 
     var name = data.drinks[0].strDrink;
-    var ingredients = data.drinks[0].ingredientsList;
     var instructions = data.drinks[0].strInstructions;
-    var image = data.drinks[0].strDrinkThumb;
-
+    // var image = data.drinks[0].strDrinkThumb;
 
     cocktailNameEl.textContent = `${name}`;
     ingredientsEl.textContent = `Ingredients: ${ingredients}`;
     instructionsEl.textContent = `Instructions: ${instructions}`;
-    imageEl.textContent = `${image}`;
-
-    console.log("Random drink!", data);
+    // imageEl.innerHTML = `${image}`;
+    
+    console.log( data);
 
 }
+
+
 
 
 
